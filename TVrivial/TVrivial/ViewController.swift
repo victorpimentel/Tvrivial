c696b4faa44fe8bde4903bd2777876025142bddb
asantos <asantos@tuenti.com>
2015-09-26 12:45:37 +0200

    Add Xcode project

---
+++ TVrivial/TVrivial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVrivial
+//
+//  Created by asantos on 26/09/15.
+//  Copyright © 2015 herodevelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
